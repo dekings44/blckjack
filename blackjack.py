6393af01629c0818025f0bda7572f0fa8d21be7f
Kingsley Ihemere <dekings44@gmail.com>
2022-06-12 20:06:41 +0100

    Blackjack project first commit

--- blackjack.py
+++ blackjack.py
@@ -22,13 +22,54 @@ def calculate_score(cards):
     return sum(cards)
 
 
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return "Draw"
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack"
+    elif user_score == 0:
+        return "Win with a Blackjack"
+    elif user_score > 21:
+        return "You went over. You lose"
+    elif computer_score > 21:
+        return "Opponent went over. You win"
+    elif user_score > computer_score:
+        return "You win"
+    else:
+        return "You lose"
+
+
 user_card = []
 computer_card = []
+is_game_over = False
 
 for _ in range(2):
     user_card.append(deal_card())
     computer_card.append(deal_card())
 
+while not is_game_over:
 
-user_score = calculate_score(user_card)
-computer_score = calculate_score(computer_card)
+    user_score = calculate_score(user_card)
+    computer_score = calculate_score(computer_card)
+
+    print(f" Your cards: {user_card}, current score: {user_score}")
+    print(f" Computer's first card is: {computer_card[0]}")
+
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        is_game_over = True
+    else:
+        user_should_deal = input("Type 'y' to get another card, type 'n' to pass:\n")
+        if user_should_deal == 'y':
+            user_card.append(deal_card())
+        else:
+            is_game_over = True
+
+
+while computer_score != 0 and computer_score < 17:
+    computer_card.append(deal_card())
+    computer_score = calculate_score(computer_card)
+
+print(f" Yout final hand: {user_card}, final score: {user_score}")
+print(f" Computer's final hand: {computer_card}, final score: {computer_score}")
+
+print(compare(user_score, computer_score))
